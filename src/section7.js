15580319441bb112ec563d72934050a042185066
rowaxl <rowaxl0@gmail.com>
2020-05-12 16:25:10 -0700

    add section7

--- src/section7.js
+++ src/section7.js
@@ -7,6 +7,17 @@ function calcDay() {
 
   const days = Math.floor((endDate - startDate) / (3600 * 1000 * 24));
 
+  if (!days) {
+    document.getElementById('year_error').style = 'display: block;';
+    document.getElementById('days_res').style = 'display: none;';
+    return;
+  }
+
+  console.log(days);
+
+  document.getElementById('year_error').style = 'display: none;';
+  document.getElementById('days_res').style = 'display: block;';
+
   const livedDays = document.getElementById('lived_days');
   livedDays.textContent = days;
 }
