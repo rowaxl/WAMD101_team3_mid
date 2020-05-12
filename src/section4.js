4ee6fc511576c272aec57ff15532b7e07129b604
rowaxl <rowaxl0@gmail.com>
2020-05-12 15:47:54 -0700

    add section5

--- src/section4.js
+++ src/section4.js
@@ -5,6 +5,7 @@ function sumSeq() {
   const length = (lastNum - initialNum + 1);
   const sum = length * ((initialNum + lastNum) / 2);
 
+  document.getElementById('seq_res').style = 'display: block;'
   if (!sum) {
     document.getElementById('seq_sum').textContent = 'Please input propper numbers!';
     document.getElementById('seq_sum').className = 'text-warning';
